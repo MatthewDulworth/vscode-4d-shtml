a1e830c1824872c525c54ff6b84c14ad9e501eaa
Matthew Dulworth <mhdulworth@gmail.com>
2026-07-15 11:05:30 -0500

    Added commented out vscode registration

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,15 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     // console.log('4d-shtml active');
+    context.subscriptions.push(
+        // vscode.languages.registerDocumentSemanticTokensProvider(selector, tokensProvider, legend),
+        // vscode.languages.registerHoverProvider(selector, hoverProvider),
+        // vscode.languages.registerDefinitionProvider(selector, defProvider),
+        // vscode.languages.registerCompletionItemProvider(selector, snippetProvider, '#'),
+        // vscode.workspace.onDidChangeTextDocument(e => scheduleValidation(e.document)),
+    );
+    // methodIndex.start(); // fire-and-forget background scan
 }
+
 
 export function deactivate() {}
